031eb1be107dc5b8535f958bd1061fc36813d3ba
Daniel Neset <danielneset@gmail.com>
2025-05-05 12:26:54 +0200

    Removed unused files

--- src/main/java/no/ntnu/gr10/bachelor_rest_api/company/Company.java
+++ src/main/java/no/ntnu/gr10/bachelor_rest_api/company/Company.java
@@ -1,7 +1,13 @@
 package no.ntnu.gr10.bachelor_rest_api.company;
 
-import jakarta.persistence.*;
-import no.ntnu.gr10.bachelor_rest_api.administrator.Administrator;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import no.ntnu.gr10.bachelor_rest_api.administrator.AdministratorCompany;
 
 import java.util.HashSet;
--- src/main/java/no/ntnu/gr10/bachelor_rest_api/company/CompanyRepository.java
+++
@@ -1,6 +0,0 @@
-package no.ntnu.gr10.bachelor_rest_api.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
